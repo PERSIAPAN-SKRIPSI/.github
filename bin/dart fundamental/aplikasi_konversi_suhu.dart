42f8a4e637178992cae8c71f5e793f9d112edcf9
DEVCODEFAISHOL <faishol2561@gmail.com>
2024-09-26 00:18:24 +0700

    testingg

---
+++ bin/dart fundamental/aplikasi_konversi_suhu.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //membuat input suhu fahrenhit
+  stdout.write('masukan suhu fahrenhit:');
+  String? inputFahrenhit =stdin.readLineSync();
+
+  //memastikan input tidak null
+
+  if (inputFahrenhit != null && double.tryParse(inputFahrenhit)!=null) {
+    double fahrenhit = double.parse(inputFahrenhit);
+
+    //mengkonversikan suhu dari fahrenhit ke celcius
+    double celcius = (5/9) *(fahrenhit -32);
+
+    print('suhu didalam celcius: ${celcius.toString()}');
+  } else {
+    
+  }
+}
